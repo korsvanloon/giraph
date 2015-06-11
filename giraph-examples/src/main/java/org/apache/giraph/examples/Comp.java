507590523aa432efd95973dc60055079a5d69c2a
Kors <korsvanloon@gmail.com>
2015-06-11 14:48:42 +0200

    try with logger

--- giraph-examples/src/main/java/org/apache/giraph/examples/Comp.java
+++ giraph-examples/src/main/java/org/apache/giraph/examples/Comp.java
@@ -3,11 +3,15 @@ package org.apache.giraph.examples;
 import org.apache.giraph.edge.Edge;
 import org.apache.giraph.graph.BasicComputation;
 import org.apache.giraph.graph.Vertex;
+import org.apache.giraph.io.formats.TextVertexOutputFormat;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapred.join.TupleWritable;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.log4j.Logger;
 
+import javax.validation.constraints.Null;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +24,9 @@ import java.util.List;
 )
 public class Comp extends BasicComputation<
         IntWritable, NullWritable, NullWritable, TupleWritable> {
+
+  private static final Logger LOG =
+          Logger.getLogger(Comp.class);
 
   @Override
   public void compute(
@@ -45,12 +52,15 @@ public class Comp extends BasicComputation<
     for(IntWritable smallerV : smallerVertices)
       for(IntWritable greaterV : greaterVertices)
         // hey greaterV, do you have a connection to smallerV? kind regards V
-        sendMessage(greaterV, new TupleWritable(new Writable[]{ smallerV, vertex.getId() }));
+        sendMessage(greaterV, new TupleWritable(new Writable[]{smallerV, vertex.getId()}));
 
     vertex.voteToHalt();
   }
 
   private void saveTriangle(TupleWritable tuple, Vertex vertex) {
-    System.out.println("[" + tuple.get(0).toString() + ", " + tuple.get(1).toString() + ", " + vertex.getId().toString() + "]");
+    String triangle = "[" + tuple.get(0).toString() + ", " + tuple.get(1).toString() + ", " + vertex.getId().toString() + "]";
+    System.out.println(triangle);
+    LOG.info(triangle);
   }
+
 }
